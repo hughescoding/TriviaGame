c452ef4f636684119ca1a220c4795f1b612129b1
hughescoding <mhughes@myjao.org>
2018-09-27 09:53:40 -0400

    logic to record correct answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,69 +2,85 @@
 //everything nested in one function
 function init() {
 
-    console.log('hello'); //Is JS Connected?
-   
-    var $game = $('#start');
-    var $countdown = $("#timer-display");
-    
-    //Variables for timer
-    var timeRemaining = 31;
-    var timerOn = false;
-    var timerId = false;
-    
-    //variables for answers
-    var numCorrect = 0
-    var answers = ["#q1true", "#q2false", "#q3false", "#q4true", "#q5true", "#q6true"]
-    
-   
+  console.log('hello'); //Is JS Connected?
+
+  var $game = $('#start');
+  var $countdown = $("#timer-display");
+
+  //Variables for timer
+  var timeRemaining = 31;
+  var timerOn = false;
+  var timerId = false;
+
+  //variables for answers
+  var numCorrect = 0
+  var answers = ["start", "q1true", "q2false", "q3false", "q4true", "q5true", "q6true"]
+  var userSelection = []
 
   //hide the answers function
   function hideAnswers() {
     $(".answers").hide();
-    }
+  }
   //show the answers function
-    function showAnswers() {
-      $(".answers").show();
-    }
- 
+  function showAnswers() {
+    $(".answers").show();
+  }
+
   $(hideAnswers);
-  
+
   //countdown function  
   function countDownLogic() {
 
-      if (timeRemaining > 0) {
-        --timeRemaining;
-        $countdown.html(timeRemaining);
-        console.log(timeRemaining);
-        
-        // when countdown hits zero
-        if (timeRemaining === 0) {
-          console.log('time is up!');
-          $(showAnswers);
-        } else {
-          console.log('Counting..');
+    if (timeRemaining > 0) {
+      --timeRemaining;
+      $countdown.html(timeRemaining);
+      console.log(timeRemaining);
+
+      // when countdown hits zero
+      if (timeRemaining === 0) {
+        alert('time is up!');
+      
+        var length = Math.min(answers.length, userSelection.length);
+        //Because my countdown logic also records the 'start' click, I had to set a negative value to get the answer count right!
+        var countMatched = -1, countNotMatched = -1;
+
+        for (var index = 0; index < length; index++) {
+          if (answers[index] == userSelection[index])
+            countMatched++;
+          else if (userSelection.indexOf(answers[index]) >= 0)
+            countNotMatched++;
         }
+        console.log(countMatched);
+        console.log(countNotMatched);
+        //I don't rember going over how to make a second line of text in alert during class, stackoverflow to the resue! \n does the trick.
+        alert('You got' + " " + countMatched + " " + "correct!" + "\nReview your answers below" )
+        $(showAnswers);
+      } else {
+        console.log('Counting..');
       }
     }
-  
-    $game.on('click', () => {
-      if (!timerOn) {
-        timerOn = true;
-        timerId = setInterval(countDownLogic, 1000);
-      }
-    });
-    
-    //Log the button click
-    $("button").click(function() {
-      console.log(this.id); 
-      var userSelection = [];
-      //userSelection[i++] = $(this).attr('id');
-      
-      
+  }
+
+  $game.on('click', () => {
+    if (!timerOn) {
+      timerOn = true;
+      timerId = setInterval(countDownLogic, 1000);
+    }
   });
 
-  }
- 
-  $(init);
-  
-  
+  //Log the button click
+  $("button").click(function () {
+    console.log(this.id);
+    userSelection.push(this.id);
+    console.log(userSelection);
+  });
+
+
+
+
+
+}
+
+$(init);
+
+
